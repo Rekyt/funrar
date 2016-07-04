e933c3ae53cf0d27ce2dd84117f30ef6d666e90f
Pierre DENELLE <Pierre DENELLE>
2016-07-04 19:50:08 +0200

    restrictedness value

--- R/restrictedness_functions.R
+++ R/restrictedness_functions.R
@@ -61,49 +61,31 @@ restrictedness = function(com_table, com, species) {
 #' Computes restrictedness from a presence-absence matrix of species with a
 #' provided functional distance matrix.
 #'
-#' Experimental for the moment, should be merged with previous function
-#' 'distictinctiveness()'
 #'
-#' @param pres_matrix a presence-absence matrix, with species in rows and sites
-#'      in columns (not containing relative abundances for the moments)
+#' @param pres_matrix a presence-absence matrix, with sites in rows and species
+#'      in columns
 #'
-#' @inheritParams uniqueness
-#'
-#' @return a similar matrix to presence-absence with restrictedness at sites?
+#' @return A tidy data.frame containing species' names and their restrictedness value
 #'
 #' @importFrom dplyr %>% bind_rows
 #'
 #' @export
-pres_restrictedness = function(pres_matrix, dist_matrix) {
+pres_restrictedness = function(pres_matrix) {
   
-  if (nrow(dist_matrix) != nrow(pres_matrix)) {
-    message("Distance matrix > number of species\nTaking subset of distance matrix")
-    dist_matrix = dist_matrix[rownames(pres_matrix), rownames(pres_matrix)]
+  # Making sure that pres_restrictedness is a matrix
+  if (is.matrix(pres_matrix)==FALSE){
+    pres_matrix <- as.matrix(pres_matrix)
+    warning("Site-species input was not a matrix.")
   }
   
-  # Matrix product of distance matrix and presence absence matrix
-  index_matrix = dist_matrix %*% pres_matrix
+  # get the total number of communities
+  n_com <- nrow(pres_matrix)
   
-  # Replace species not present in communities
-  index_matrix[which(pres_matrix == 0)] = NA
+  # Compute the sum of all species' occurrences divided by n_com
+  occupancy <- colSums(pres_matrix, na.rm = TRUE) / n_com
   
-  # Compute sum of relative abundances
-  if (requireNamespace("Matrix", quietly = TRUE) & is(pres_matrix, "sparseMatrix")) {
-    total_sites = Matrix::colSums(pres_matrix)
-  } else {
-    total_sites = colSums(pres_matrix)
-  }
+  # Format occupancy in data.frame
+  occupancy <- data.frame("sp" = names(occupancy), "Ri" = as.numeric(occupancy))
   
-  # Subtract focal species value to site total
-  # /!\ need to Transpose because applying function to row tranposes matrix
-  denom_matrix = t(apply(pres_matrix, 1, function(x) total_sites - x))
-  
-  index_matrix = index_matrix / denom_matrix
-  
-  if (length(sum(is.nan(index_matrix)))) {
-    warning("Some communities had a single species in them\nComputed value assigned to 'NaN'")
-  }
-  
-  
-  return(index_matrix)
+  return(occupancy)
 }
