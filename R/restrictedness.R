a68825b74206c6f45e35c6d27ef3a28b2b093e45
Rekyt <matthias.grenie@hotmail.com>
2016-07-05 11:59:28 +0200

    Modified restrictedness help function

--- R/restrictedness.R
+++ R/restrictedness.R
@@ -5,60 +5,63 @@
 #
 #
 
-#' Compute restrictedness for every species ------------------------------
+#' Table Restrictedness
 #'
-#' @param com_table, a tidy data.frame of community with column with species
+#' Compute restrictedness for every species
 #'
-#' com, a character vector indicating the name of community column
+#' @param com_table a tidy data.frame of community with column with species
 #'
-#' species, a character vector indicating the name of species column
+#' @param com a character vector indicating the name of community column
+#'
+#' @param species a character vector indicating the name of species column
 #'
 #'
-#' @return A tidy data.frame containing species' names and their restrictedness value
+#' @return A tidy data.frame containing species' names and their restrictedness
+#'    value
 #' @export
 restrictedness = function(com_table, com, species) {
-  
+
   # Test to be sure
   if ((is.character(com)) == FALSE) {
     stop("com has to be a character string.")
   }
-  
+
   if ((is.character(species)) == FALSE) {
     stop("species has to be a character string.")
   }
-  
+
   if ((com %in% colnames(com_table)) == FALSE) {
     stop("Community table does not have any communities.")
   }
-  
+
   if ((species %in% colnames(com_table)) == FALSE) {
     stop("Community table does not have any species.")
   }
-  
+
   if (!is.character(com_table[[species]])) {
     stop("Provided species are not character.")
   }
-  
+
   if (!is.character(com_table[[com]])) {
     stop("Provided communities are not character.")
   }
-  
+
   # get the total number of communities
-  n_com <- length(unique(com_table[, com]))
-  
+  n_com = length(unique(com_table[, com]))
+
   # Compute the sum of all species' occurrences divided by n_com
-  occupancy <- table(com_table[, species]) / n_com
-  
+  occupancy = table(com_table[, species]) / n_com
+
   # Format occupancy in data.frame
-  occupancy <- data.frame("sp" = names(occupancy), "Ri" = as.numeric(occupancy))
-  
-  occupancy$Ri <- 1 - occupancy$Ri
-  
+  occupancy = data.frame("sp" = names(occupancy), "Ri" = as.numeric(occupancy))
+
+  occupancy$Ri = 1 - occupancy$Ri
+
   return(occupancy)
 }
 
 
-#' restrictedness on presence/absence matrix
+#' Restrictedness on presence/absence matrix
 #'
 #' Computes restrictedness from a presence-absence matrix of species with a
 #' provided functional distance matrix.
@@ -69,25 +72,23 @@ restrictedness = function(com_table, com, species) {
 #'
 #' @return A tidy data.frame containing species' names and their restrictedness value
 #'
-#' @importFrom dplyr %>% bind_rows
-#'
 #' @export
 pres_restrictedness = function(pres_matrix) {
-  
+
   # Making sure that pres_restrictedness is a matrix
-  if (is.matrix(pres_matrix)==FALSE){
-    pres_matrix <- as.matrix(pres_matrix)
+  if (is.matrix(pres_matrix) == FALSE) {
+    pres_matrix = as.matrix(pres_matrix)
     warning("Site-species input was not a matrix.")
   }
-  
+
   # get the total number of communities
-  n_com <- nrow(pres_matrix)
-  
+  n_com = nrow(pres_matrix)
+
   # Compute the sum of all species' occurrences divided by n_com
-  occupancy <- colSums(pres_matrix, na.rm = TRUE) / n_com
-  
+  occupancy = colSums(pres_matrix, na.rm = TRUE) / n_com
+
   # Format occupancy in data.frame
-  occupancy <- data.frame("sp" = names(occupancy), "Ri" = as.numeric(occupancy))
-  
+  occupancy = data.frame("sp" = names(occupancy), "Ri" = as.numeric(occupancy))
+
   return(occupancy)
 }
