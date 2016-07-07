e175855a17f70600a93884cf02f935403b986a81
Rekyt <matthias.grenie@hotmail.com>
2016-07-07 10:36:52 +0200

    Error messages for uniqueness

--- tests/testthat/test-rarity_indices.R
+++ tests/testthat/test-rarity_indices.R
@@ -127,6 +127,12 @@ test_that("Correct Uniqueness computation", {
   expect_equivalent(table_uniqueness(com_table[1:2, ], "species", dist_mat),
                     valid_ui)
 
+  expect_error(table_uniqueness(com_table[1:2, ], "NOT_IN_TABLE", dist_mat),
+    regexp = "'NOT_IN_TABLE' species column not in column names")
+
+  expect_error(table_uniqueness(com_table[1:2, ], "NOT_IN_TABLE", dist_mat[1,]),
+               regexp = "Distance matrix is not square.")
+
   expect_equivalent(table_uniqueness(com_table, "species", dist_mat), all_ui)
 
   expect_equal(uniqueness(valid_mat, dist_mat), all_ui)
