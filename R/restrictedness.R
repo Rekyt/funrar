44ac0c76a576c49ed7f94cc0a598d90d5b60f677
Pierre DENELLE <pierre.denelle@cefe.cnrs.fr>
2016-07-08 18:50:56 +0200

    Plots added to vignette. Restrictedness corrected.

--- R/restrictedness.R
+++ R/restrictedness.R
@@ -98,6 +98,10 @@ restrictedness = function(pres_matrix) {
 
   # get the total number of communities
   n_com = nrow(pres_matrix)
+  
+  # Convert all the matrix values into 0/1
+  pres_matrix[is.na(pres_matrix)==T] = 0
+  pres_matrix[pres_matrix > 0] = 1
 
   # Compute the sum of all species' occurrences divided by n_com
   occupancy = colSums(pres_matrix, na.rm = TRUE) / n_com
