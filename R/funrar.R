db75e1e6790169d3277f9ea14c1a8641be5ffe18
Rekyt <matthias.grenie@hotmail.com>
2016-07-11 16:14:31 +0200

    Added 'funrar_stack()' function

--- R/funrar.R
+++ R/funrar.R
@@ -54,3 +54,48 @@ funrar = function(pres_matrix, dist_matrix, rel_abund = FALSE) {
                 Ri = geog_rest))
   }
 }
+
+#' Functional Rarity indices for data.frame
+#'
+#' From a stack community data.frame
+#'
+#' @inheritParams distinctiveness_stack
+#'
+#' @return A list of 3 objects (or 4 if \code{rel_abund = TRUE}):
+#'    \describe{
+#'      \item{\strong{Ui}}{a vector containing uniqueness values per species,}
+#'      \item{\strong{Di}}{a site-species matrix with functional distinctiveness
+#'        values per species per site,}
+#'      \item{\strong{Ri}}{a vector containing geographical restrictedness values
+#'        per species,}
+#'    }
+#'    and if \code{rel_abund = TRUE},
+#'    \describe{
+#'      \item{\strong{Si}}{a site-species matrix with scarcity values per
+#'        species per site.}
+#'    }
+#'
+#' @export
+funrar_stack = function(com_df, sp_col, com, abund = NULL,
+                        dist_matrix) {
+
+  funct_dist = distinctiveness_stack(com_df, sp_col, com, abund, dist_matrix)
+  funct_uniq = uniqueness_stack(com_df, sp_col, dist_matrix)
+  geog_rest = restrictedness_stack(com_df, sp_col, dist_matrix)
+
+  if (is.null(abund)) {
+    return(list(
+      Ui = funct_uniq,
+      Di = funct_dist,
+      Ri = geog_rest
+    ))
+  } else {
+    scar = scarcity_stack(com_df, sp_col, com, abund)
+    return(list(
+      Ui = funct_uniq,
+      Di = funct_dist,
+      Ri = geog_rest,
+      Si = scar
+    ))
+  }
+}
